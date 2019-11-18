757084328784ba4a3455429ac0b4d487adc95215
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-18 18:12:24 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,6 +30,7 @@ def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 }
     m.title = args[:title]
     m.release_date = args[:release_date]
     m.save
+    m
   end
 end
 
